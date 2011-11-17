a0e5c09cc69adbb1d0db3acc66f15e30da314049
Saurabh Jain <saurabhjain.net@gmail.com>
2011-11-17 13:46:46 -0800

    Committing first changes

---
+++ src/com/example/photopicker/PhotoPickerActivity.java
@@ -0,0 +1,31 @@
+package com.example.photopicker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PhotoPickerActivity extends Activity {
+	
+	private Button mPickPhotos;
+	final Context context = this;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mPickPhotos = (Button)findViewById(R.id.pickPhotos);
+        
+        mPickPhotos.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final Intent intent = new Intent(context, Photos.class);
+				context.startActivity(intent);
+			}
+		});
+    }
+}
---
+++ src/com/example/photopicker/Photos.java
@@ -0,0 +1,30 @@
+package com.example.photopicker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class Photos extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.photos);
+		
+		GridView gridview = (GridView)findViewById(R.id.gridView);
+		gridview.setAdapter(new ImageAdapter(this));
+		
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				Toast.makeText(Photos.this, "" + position, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+}
