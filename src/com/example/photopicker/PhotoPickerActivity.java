8906fdffa0877f2ca1f69c7c64586eaae2aa500d
Saurabh Jain <saurabhjain.net@gmail.com>
2011-11-21 14:48:34 -0800

    Inflated GridViewfrom resources temporarily to test

--- src/com/example/photopicker/PhotoPickerActivity.java
+++ src/com/example/photopicker/PhotoPickerActivity.java
@@ -5,7 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class PhotoPickerActivity extends Activity {
@@ -22,8 +28,10 @@ public class PhotoPickerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // Attaching Pick Photos button' View 
         mPickPhotos = (Button)findViewById(R.id.pickPhotos);
-
+        
+        // OnClickListener for Pick Photos button
         mPickPhotos.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -31,6 +39,20 @@ public class PhotoPickerActivity extends Activity {
 				startActivityForResult(intent, requestCode);
 			}
 		});
+        
+        // Setting up the GridView
+        GridView gridview = (GridView)findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+        
+        // Listener for GridView
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				
+			}
+        });
     }
     
     @Override
@@ -38,5 +60,52 @@ public class PhotoPickerActivity extends Activity {
     	ifilename = data.getStringExtra("filename");
     	idataId = data.getLongExtra("dataUid", 0);
     	Toast.makeText(this, "Result Code, Filename, imageId:"+ resultCode + ", " + ifilename + ", " + idataId, Toast.LENGTH_LONG).show();
+    }
+    
+    private class ImageAdapter extends BaseAdapter {
+    	
+    	private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+		@Override
+		public int getCount() {
+			return mThumbIds.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+	    // create a new ImageView for each item referenced by the Adapter
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        ImageView imageView;
+	        if (convertView == null) {  // if it's not recycled, initialize some attributes
+	            imageView = new ImageView(mContext);
+	            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	            imageView.setPadding(8, 8, 8, 8);
+	        } else {
+	            imageView = (ImageView) convertView;
+	        }
+
+	        imageView.setImageResource(mThumbIds[position]);
+	        return imageView;
+	    }
+
+	    // references to our images
+	    private Integer[] mThumbIds = {
+	    		R.drawable.ic_launcher
+	    };
+    	
     }
 }
